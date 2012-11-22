ad388daafd10be8f5a5828a7c56dbf72dfb0b41b
Jeremy Prime <jeremy.prime@millross-consultants.com>
2012-11-22 22:11:05 +0000

    Partial migrate of test case to Groovy from Java to cut out boilerplate around Handler interfaces

---
+++ vertxRestService/src/test/java/com/millross/vertx/rest/test/DemoGroovyTest.groovy
@@ -0,0 +1,67 @@
+package com.millross.vertx.rest.test
+
+import org.vertx.java.core.json.JsonObject
+
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+/**
+ */
+class DemoGroovyTest extends GroovyTestBase {
+
+    void test() {
+        startApp("test_restserver.js", 1);
+        println 'App started'
+
+        def client = vertx.createHttpClient().setHost("localhost").setPort(8080);
+        def done = new CountDownLatch(2);
+
+        def request = client.post("/customer/create") {resp ->
+            println(resp.statusCode)
+            resp.bodyHandler {buffer ->
+                String custId = buffer.toString()
+                println ("Customer id = " + custId)
+                getCustomerWithId(client, done, custId);
+            }
+            done.countDown();
+        }
+
+        request.setChunked(true);
+        request.headers["Accept"] = "application/json"
+        request.write(new JsonObject(createCustomerMap()).toString());
+        request.end();
+
+        done.await(1000, TimeUnit.MILLISECONDS);
+        System.out.println("About to stop app");
+
+
+    }
+
+    void getCustomerWithId(client, done, id) {
+        println("Get: Customer id = " + id);
+        def request = client.get("/customer/" + id) { resp ->
+            println(resp.statusCode);
+            resp.bodyHandler { buffer ->
+                println("Response body is " + buffer.toString());
+            }
+            done.countDown();
+        }
+
+        request.headers["Accept"] =  "application/json, text/html"
+        request.end();
+    }
+
+    void testSomething() {
+        assert 1 == 1
+        assert 2 + 2 == 4 : "We're in trouble, arithmetic is broken"
+    }
+
+    private Map createCustomerMap() {
+        Map customerMap = new HashMap();
+        customerMap.put("name", "Joe Dolce");
+        customerMap.put("address", "6500 Warwick Avenue");
+        customerMap.put("status", "elevated");
+        return customerMap;
+    }
+
+}
